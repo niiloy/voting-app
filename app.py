fda0905466135dbcb018e5f1e07106895e1c128b
kamrul <kamrul@ip-172-31-24-38.ap-south-1.compute.internal>
2025-07-25 02:23:39 +0000

    redis fix and container id showing

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template, request, redirect, url_for
 import redis
+import time
 
 app = Flask(__name__)
 
 # Connect to Redis
-r = redis.Redis(host='redis', port=6379, db=0, decode_responses=True)
+while True:
+    try:
+        r = redis.Redis(host="redis", port=6379, db=0)
+        if r.ping():
+            print("Connected to Redis!")
+            break
+    except redis.exceptions.ConnectionError:
+        print("Waiting for Redis...")
+        time.sleep(1)
+#r = redis.Redis(host='redis', port=6379, db=0, decode_responses=True)
 
 # Initialize vote keys if not present
 if not r.exists("votes:cats"):
@@ -19,13 +29,15 @@ def index():
         if vote in ['cats', 'dogs']:
             r.incr(f'votes:{vote}')
         return redirect(url_for('results'))
-    return render_template('index.html')
+    container_id = os.uname()[1]
+    return render_template('index.html', container_id=container_id)
 
 @app.route('/results')
 def results():
     cats = int(r.get("votes:cats") or 0)
     dogs = int(r.get("votes:dogs") or 0)
-    return render_template('results.html', votes={'cats': cats, 'dogs': dogs})
+    container_id = os.uname()[1]
+    return render_template('results.html', votes={'cats': cats, 'dogs': dogs}, container_id=container_id)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
